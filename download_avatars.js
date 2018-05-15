58f087cbd216b78303b1c5d3dc2e60836687e2dc
Chris Kim <chriskihyun@hotmail.com>
2018-05-15 03:46:47 +0000

    first commit

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,18 @@
+var ss =process.argv
 var request = require('request');
+var fs = require('fs');
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  var callContributors = JSON.parse(result);
-  callContributors.forEach(function(contributor){
-    console.log(contributor.avatar_url);
+if(process.argv.length === 4){
+  getRepoContributors(process.argv[2], process.argv[3], function(err, result) {
+    var callContributors = JSON.parse(result);
+    callContributors.forEach(function(contributor){
+      downloadImageByURL(contributor.avatar_url, "./avatars/"+contributor.login+".jpg");
+    })
   })
-});
+}else{
+  console.log("you did not put two arguments")
+};
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
@@ -15,10 +21,18 @@ function getRepoContributors(repoOwner, repoName, cb) {
       'User-Agent': 'request'
     }
   };
-  request(options, function(err, res, body) {
+  request(options, function(err, res, body){
     cb(err, body);
   });
 }
 
+
+function downloadImageByURL(url, filePath) {
+request.get(url)
+       .on('error', function (err) {
+         throw err;
+       })
+       .pipe(fs.createWriteStream(filePath));
+}
 
 
